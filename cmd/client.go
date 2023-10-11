82fc2f4df6d4cbfcd0e041e1ba8461c3ad1e9783
atgane <hyper2012@naver.com>
2023-10-11 22:16:06 +0900

    주석 수정

--- cmd/client.go
+++ cmd/client.go
@@ -12,16 +12,19 @@ import (
 var tracer trace.Tracer
 
 func main() {
+	// 2.1. trace provider를 가져옵니다.
 	tp, err := internal.InitTrace()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// 2.2. tracer를 생성합니다.
 	tracer = tp.Tracer("client-go")
 	ctx := context.Background()
 	someFunc1(ctx)
 }
 
+// 2.3. 컨텍스트를 전파합니다.
 func someFunc1(ctx context.Context) {
 	ctx, span := tracer.Start(ctx, "some func1")
 	defer span.End()
